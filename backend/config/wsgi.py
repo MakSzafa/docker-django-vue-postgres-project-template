bb46e96a8a0b350c422a08eb490ec3a2bb50b3f8
Max Szafarski <maksymilian.szafarski@vocale.app>
2022-12-07 17:20:48 +0100

    update docker related files, scripts, add nginx

--- backend/config/wsgi.py
+++ backend/config/wsgi.py
@@ -8,19 +8,11 @@ this application via the ``WSGI_APPLICATION`` setting.
 """
 
 import os
-import sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-# This allows easy placement of apps within the interior serenity directory.
-current_path = os.path.dirname(os.path.abspath(__file__)).replace('/config', '')
-sys.path.append(current_path)
-sys.path.append(os.path.join(current_path, 'apps'))
 
-# This application object is used by any WSGI server configured to use this
-# file. This includes Django's development server, if the WSGI_APPLICATION
-# setting points here.
 application = get_wsgi_application()
 
 
