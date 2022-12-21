e6de16eea18454b9f8720f756ce186f26bc02b7b
Max Szafarski <maksymilian.szafarski@vocale.app>
2022-12-21 19:26:55 +0100

    add default pagination

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -188,7 +188,10 @@ REST_FRAMEWORK = {
         'rest_framework.parsers.FormParser',
         'rest_framework.parsers.MultiPartParser',
         'rest_framework.parsers.FileUploadParser'
-    ]
+    ],
+    # Add default pagination, it can be overriden for specific generic views
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 20
 }
 
 SIMPLE_JWT = {
