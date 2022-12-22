32b008aabde0a3a05baa42f71159377f2d3b5536
Max Szafarski <maksymilian.szafarski@vocale.app>
2022-12-22 22:35:02 +0100

    change api endpoints

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from apps.users.views import UserViewSet
@@ -15,11 +15,9 @@ router = routers.DefaultRouter(trailing_slash = False)
 # register apps here
 router.register(r'users', UserViewSet)
 
-
-urlpatterns = router.urls
-
-urlpatterns += [
+urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
