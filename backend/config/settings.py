2de056f7c4294988343afaec4def0ae4ec41a17d
Max Szafarski <makszafa@gmail.com>
2023-03-09 13:32:55 +0100

    add CSRF_TRUSTED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -67,6 +67,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # DOMAINS
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
 CORS_ALLOWED_ORIGINS = env.list('CORS_ALLOWED_ORIGINS')
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS')
 
 # EMAIL CONFIGURATION
 # ------------------------------------------------------------------------------
